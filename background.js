b2fa662ccc38bf69b76dd142959da6173efd0ac2
wukgdu <wukgdu365@gmail.com>
2021-09-19 16:07:16 +0800

    small change in matched url

--- background.js
+++ background.js
@@ -2,12 +2,10 @@ chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
   // console.log(details)
   if (details.url == "https://bbs.pku.edu.cn/v2/userstat.php") {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.scripting.executeScript(
-        {
-          target: {tabId: tabs[0].id},
-          files: ["d3.v7.js", "show.js"]
-        }
-      )
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        files: ["d3.v7.js", "show.js"]
+      })
       // chrome.tabs.sendMessage(tabs[0].id, { greeting: "hello" }, function (response) {
       //   console.log(response.farewell);
       // });
